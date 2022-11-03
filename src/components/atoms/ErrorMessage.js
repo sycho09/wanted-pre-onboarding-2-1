6ae8af2a0fee57efaadea23fb55346566b968872
sycho09 <wyswid@naver.com>
2022-11-03 10:28:34 +0900

    style: add error messag style

---
+++ src/components/atoms/ErrorMessage.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ErrorMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 120px);
+  font-size: 1.8rem;
+`;
+
+export default ErrorMessage;
