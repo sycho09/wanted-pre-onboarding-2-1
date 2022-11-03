30de0639cbc1d29fade378caac85a8e69e56ce26
sycho09 <wyswid@naver.com>
2022-11-03 11:24:59 +0900

    chore: change car info texts readable in car description page

--- src/components/templates/CarDescription.js
+++ src/components/templates/CarDescription.js
@@ -8,6 +8,7 @@ import InfoText from '../atoms/InfoText';
 import TitleBox from '../atoms/TitleBox';
 import SubTitle from '../atoms/SubTitle';
 import DetailInfoBox from '../molecules/DetailInfoBox';
+import { FUEL_TYPE, SEGMENT } from '../../utils/types';
 
 export default function CarDescription({ ...selected }) {
   const {
@@ -30,11 +31,11 @@ export default function CarDescription({ ...selected }) {
       <TitleBox>
         <h6>{brand}</h6>
         <h5>{name}</h5>
-        <InfoText>월 {amount} 원</InfoText>
+        <InfoText>월 {amount.toLocaleString()} 원</InfoText>
       </TitleBox>
       <SubTitle>차량 정보 </SubTitle>
-      <DetailInfoBox label='차종' text={fuelType} />
-      <DetailInfoBox label='연료' text={segment} />
+      <DetailInfoBox label='차종' text={FUEL_TYPE[fuelType]} />
+      <DetailInfoBox label='연료' text={SEGMENT[segment]} />
       <DetailInfoBox
         label='이용 가능일'
         text={`${month}월 ${getdate}일 (${dates[day]})부터`}
@@ -45,7 +46,10 @@ export default function CarDescription({ ...selected }) {
       ))}
       <SubTitle>추가상품</SubTitle>
       {additionalProducts.map((el) => (
-        <DetailInfoBox label={el.name} text={`월 ${el.amount}만원`} />
+        <DetailInfoBox
+          label={el.name}
+          text={`월 ${el.amount.toLocaleString()}만원`}
+        />
       ))}
     </DescriptionWrapper>
   );
