7d129620c5b11facf28cf359de9c5239114f7582
sycho09 <wyswid@naver.com>
2022-11-03 10:21:08 +0900

    feat: car list page

--- src/pages/CarList.js
+++ src/pages/CarList.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useThemeContext } from '../utils/ThemeContext';
+import useGetList from '../utils/hooks/useGetList';
 
-export default function CarList() {
-  return <div>CarList</div>;
+function CarList() {
+  const { list } = useThemeContext();
+  const { isLoading, isSuccess, getCarList } = useGetList();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getCarList();
+  }, []);
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(isLoading);
+      console.log('list', list);
+    }
+  }, [isSuccess]);
+
+  return (
+    <>
+      {isLoading && <p>불러오는 중입니다</p>}
+      {!isLoading && isSuccess && (
+        <div style={{ padding: '2rem 1rem' }}>
+          {list.map((el) => (
+            <>
+              <button type='button' onClick={() => navigate(`/${el.id}`)}>
+                이동하기
+              </button>
+              <p>{el.attribute.brand}</p>
+              <p>{el.attribute.name}</p>
+              <p>{el.attribute.segment}</p>
+              <p>{el.attribute.fuelType}</p>
+              <img alt={el.id} src={el.attribute.imageUrl} width='100px' />
+              <p>{el.amount}</p>
+            </>
+          ))}
+        </div>
+      )}
+
+      {!isLoading && isSuccess && list.length < 1 && <p>차량이 없습니다</p>}
+    </>
+  );
 }
+
+export default CarList;
